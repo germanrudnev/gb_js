cbbd1434175aee62e7ab41151a9935bbd657bddd
German Rudnev <germanrudnev@ya.ru>
2019-06-21 23:29:22 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*var name = prompt("Введите ваше имя", "Имя вводится здесь");
+alert("Здравствуйте, "+name);*/
+document.write("<h1>Заголовок</h1>");
+console.log("Привет из консоли");
+
+var list = "<ul>";
+list += "<li>Элемент 1</li>";
+list += "<li>Элемент 1</li>";
+list += "<li>Элемент 1</li>";
+list += "<li>Элемент 1</li>";
+list += "</ul>";
+document.write(list);
+
